5a884a316af2906eb6f95a8cbb37ea48d4b1c0c0
Katie Emelianova <katieemelianova@Mac.telekom.ip>
2025-12-27 22:08:01 +0100

    more

--- analyse_photosynq.R
+++ analyse_photosynq.R
@@ -11,7 +11,8 @@ psynq <- read_delim("/Users/katieemelianova/Desktop/Spartina/France2025/photosyn
   dplyr::select(LEF, NPQt, PhiNPQ, Phi2, "Light Intensity (PAR)", PhiNO, `Which sample number?`, `Which species?`, `Which locality?`) %>%
   rename(sample_id = `Which sample number?`,
          species = `Which species?`,
-         locality = `Which locality?`)
+         locality = `Which locality?`,
+         PAR = `Light Intensity (PAR)`)
 
 mean_psynq <- psynq %>% 
   group_by(sample_id) %>%
@@ -28,15 +29,11 @@ mean_psynq <- psynq %>%
 
 plot(mean_psynq$LEF, mean_psynq$PAR)
 
-mean_psynq %>% 
-  ggplot(aes(x=PAR, y=LEF, colour=species)) +
-  geom_point(size=4) +
-  ylim(30, 60) +
-  xlim(110, 180)
 
 
-
-# phinpq vs ph2
+####################################
+#       phinpq vs ph2.             #
+####################################
 # linear inverse realtionship
 # test per species of the pearson correlation coefficient. it looks to be about the same for all three species
 # once you have established a good linear relationship for each species, you can use phi2 to perform an ANOVA
@@ -48,26 +45,49 @@ mean_psynq %>%
   ylim(0.4, 0.9) +
   xlim(-0.1, 0.4)
 
-ggdensity(mean_psynq$Phi2)
+
+###############################################################################################
+#          test for significanct difference betwen Phi2 between alterniflora and maritima    #
+###############################################################################################
+
+
+
 shapiro.test(mean_psynq$Phi2)
 # reflect and sqrt normalise phi2 to make it normal
+# I used the shapiro test to test for normality and did a density plot to look for normal distribution
 mean_psynq %<>% drop_na() %>% filter(Phi2 > 0) %>% mutate(reflect=sqrt(max(Phi2) - Phi2))
 
-t.test(x = mean_psynq %>% filter(species == "maritima") %>% pull(PhiNPQ), 
-       y = mean_psynq %>% filter(species == "alterniflora") %>% pull(PhiNPQ), 
+mean_psynq %>% filter(species == "maritima") %>% pull(reflect) %>% length()
+mean_psynq %>% filter(species == "alterniflora") %>% pull(reflect) %>% length()
+mean_psynq %>% filter(species == "anglica") %>% pull(reflect) %>% length()
+
+# do a t test for difference between species - pairwise
+t.test(x = mean_psynq %>% filter(species == "maritima") %>% pull(reflect), 
+       y = mean_psynq %>% filter(species == "alterniflora") %>% pull(reflect), 
        alternative = c("two.sided"))
 
-mean_psynq %>% 
-  filter(Phi2 > 0 & species %in% c("maritima", "alterniflora")) %>%
-  mutate(reflect=sqrt(max(Phi2) - Phi2)) %>% 
-  oneway.test(PhiNPQ ~ species,
-              data = .,
-              var.equal = TRUE)
+
+####################################
+#       LEF vs PAR             #
+####################################
 
 
 mean_psynq %>% 
-  filter(species %in% c("alterniflora", "anglica")) %>% 
-  t_test(PhiNPQ ~ species)
+  ggplot(aes(x=PAR, y=LEF, colour=species)) +
+  geom_point(size=4) +
+  ylim(30, 60) +
+  xlim(110, 180)
+
+
+mean_psynq %>%
+  group_by(species) %>%
+  summarise(correl = cor(LEF, PAR))
+
+
+
+
+
+
 
 mean_psynq %>% filter(species == "maritima") %>% pull(Phi2) %>% shapiro.test
 
