ddc0a9ea122c8eabc99d4337377570de689d58fa
Katie Emelianova <katieemelianova@Mac.telekom.ip>
2025-12-28 09:28:17 +0100

    more

--- analyse_photosynq.R
+++ analyse_photosynq.R
@@ -20,14 +20,13 @@ mean_psynq <- psynq %>%
             NPQt=mean(NPQt),
             PhiNPQ=mean(PhiNPQ),
             Phi2=mean(Phi2),
-            PAR=mean(`Light Intensity (PAR)`),
+            PAR=mean(PAR),
             PhiNO=mean(PhiNO)) %>%
   left_join((psynq %>% dplyr::select(sample_id, species, locality)), 
             by="sample_id") %>% 
   unique()
 
 
-plot(mean_psynq$LEF, mean_psynq$PAR)
 
 
 
@@ -36,8 +35,8 @@ plot(mean_psynq$LEF, mean_psynq$PAR)
 ####################################
 # linear inverse realtionship
 # test per species of the pearson correlation coefficient. it looks to be about the same for all three species
-# once you have established a good linear relationship for each species, you can use phi2 to perform an ANOVA
-# to perform anova though you need to check for normality etc and assumptions of test are satisfied
+# once you have established a good linear relationship for each species, you can use phi2 to perform a t-test for each species pair
+# need to first transform dta to meet normality assumptions
 
 mean_psynq %>% 
   ggplot(aes(x=PhiNPQ, y=Phi2, colour=species)) +
@@ -45,25 +44,46 @@ mean_psynq %>%
   ylim(0.4, 0.9) +
   xlim(-0.1, 0.4)
 
+mean_psynq %>%
+  summarise(correl = cor(Phi2, PhiNPQ))
+# -0.93 correlation pearson for all species combined
+# because there is a good inveerse relationship between Phi2 and PhiNPQ we can 
+# use Phi2 to test for any significant differences between species
 
-###############################################################################################
-#          test for significanct difference betwen Phi2 between alterniflora and maritima    #
-###############################################################################################
 
 
+##################################################################################
+#          test for significanct difference betwen Phi2 between species pairs    #
+##################################################################################
 
-shapiro.test(mean_psynq$Phi2)
+# first use the shapiro test to see if Phi2 is significantly different from normal distribution, removing outliers
+mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% pull(Phi2) %>% shapiro.test
+mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% pull(Phi2) %>% densityPlot()
+
+# yes it is, so need to transform
 # reflect and sqrt normalise phi2 to make it normal
-# I used the shapiro test to test for normality and did a density plot to look for normal distribution
-mean_psynq %<>% drop_na() %>% filter(Phi2 > 0) %>% mutate(reflect=sqrt(max(Phi2) - Phi2))
+mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% mutate(transformed_phi2=sqrt(max(Phi2) - Phi2)) %>% pull(transformed_phi2) %>% shapiro.test 
+mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% mutate(transformed_phi2=sqrt(max(Phi2) - Phi2)) %>% pull(transformed_phi2) %>% densityPlot()
 
-mean_psynq %>% filter(species == "maritima") %>% pull(reflect) %>% length()
-mean_psynq %>% filter(species == "alterniflora") %>% pull(reflect) %>% length()
-mean_psynq %>% filter(species == "anglica") %>% pull(reflect) %>% length()
+# lock in the new transformed values
+mean_psynq %<>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% mutate(transformed_phi2=sqrt(max(Phi2) - Phi2))
 
 # do a t test for difference between species - pairwise
-t.test(x = mean_psynq %>% filter(species == "maritima") %>% pull(reflect), 
-       y = mean_psynq %>% filter(species == "alterniflora") %>% pull(reflect), 
+
+# maritima - alterniflora
+t.test(x = mean_psynq %>% filter(species == "maritima") %>% pull(transformed_phi2), 
+       y = mean_psynq %>% filter(species == "alterniflora") %>% pull(transformed_phi2), 
+       alternative = c("two.sided"))
+
+# maritima - anglica
+t.test(x = mean_psynq %>% filter(species == "maritima") %>% pull(transformed_phi2), 
+       y = mean_psynq %>% filter(species == "anglica") %>% pull(transformed_phi2), 
+       alternative = c("two.sided"))
+
+
+# alterniflora - anglica
+t.test(x = mean_psynq %>% filter(species == "anglica") %>% pull(transformed_phi2), 
+       y = mean_psynq %>% filter(species == "alterniflora") %>% pull(transformed_phi2), 
        alternative = c("two.sided"))
 
 
