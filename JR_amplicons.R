fbecb64918e5e2023eee2ab6e8fe3d4661263a55
Katie Emelianova <katieemelianova@Mac.telekom.ip>
2025-12-30 21:18:09 +0100

    more

--- JR_amplicons.R
+++ JR_amplicons.R
@@ -1,3 +1,11 @@
+library(dada2)
+library(dplyr)
+library(magrittr)
+library(readr)
+library(stringr)
+library(tidyverse)
+library(phyloseq)
+
 
 # I took this from the JMF code, it just alters the filenames
 convert_to_filtered_file_name <- function(x, reads_dir) {
@@ -75,16 +83,18 @@ assign_taxonomy_jr <- function(seqtab) {
     multithread=12
   )
   taxa_jr <- addSpecies(taxa_jr, "/lisc/scratch/spartina/spartina_amplicon_code/silva_v138.2_assignSpecies.fa.gz")
-  write.table(taxa_jr, "taxonomy_species_america", quote=FALSE)
+  write.table(taxa_jr, "/Users/katieemelianova/Desktop/Spartina/spartina_amplicon_code/taxonomy_species_america", quote=FALSE)
 }
 
 
 
 sequence_table <- run_jr_analysis()
+# not to be run locally
+#assign_taxonomy_jr(sequence_table)
 
 
 
-taxonomy_species_america <- read_delim("taxonomy_species_america") %>% set_colnames(c("sequence", "Domain", "Phylum", "Class", "Order", "Family")) %>% column_to_rownames("sequence") %>% separate(Family, sep=" ", c("Family", "Genus"), extra="merge")
+taxonomy_species_america <- read_delim("/Users/katieemelianova/Desktop/Spartina/spartina_amplicon_code/taxonomy_species_america") %>% set_colnames(c("sequence", "Domain", "Phylum", "Class", "Order", "Family")) %>% column_to_rownames("sequence") %>% separate(Family, sep=" ", c("Family", "Genus"), extra="merge")
 samplesheet <- read_delim("/Users/katieemelianova/Desktop/Spartina/spartina_amplicon_code/JR_metadata.csv")
 
 metadata_jr <- samplesheet %>% dplyr::select(Run, HOST, isolation_source) %>% 
@@ -103,10 +113,100 @@ metadata_jr <- samplesheet %>% dplyr::select(Run, HOST, isolation_source) %>%
 
 
 
-OTU_JR <- otu_table(seqtab.nochim_jr, taxa_are_rows=FALSE)
+OTU_JR <- otu_table(sequence_table, taxa_are_rows=FALSE)
 TAX_JR <- tax_table(taxonomy_species_america) %>% set_rownames(rownames(taxonomy_species_america)) %>% set_colnames(c("Domain", "Phylum", "Class", "Order", "Family", "Genus"))
 sample_names(OTU_JR) <- str_remove(sample_names(OTU_JR), "_1.filtered.fastq.gz")
 
 ps_jr <- phyloseq(OTU_JR, 
                   sample_data(metadata_jr), 
                   TAX_JR)
+
+
+#######################################################################################################################
+#       after running all above code, I save RDS and this can e reloaded and can start analysis from there            #
+#######################################################################################################################
+
+saveRDS(ps_jr, "/Users/katieemelianova/Desktop/Spartina/Spartina2025/JR_amplicons.rds")
+
+##############################################################
+##############################################################
+#   Begin analysis here unless you want to rerun something   #
+##############################################################
+##############################################################
+
+
+phylo_rolando <- readRDS("/Users/katieemelianova/Desktop/Spartina/Spartina2025/JR_amplicons.rds")
+phylo_rolando <- tax_filter(
+  phylo_rolando,
+  min_prevalence = 10,
+  prev_detection_threshold = 5,
+  min_total_abundance = 5,
+  min_sample_abundance = 5)
+
+
+phylo_rolando@sam_data$study <- "usa"
+phylo_rennes@sam_data$study <- "eur"
+
+phylo_rennes_prop <- transform_sample_counts(phylo_rennes, function(otu) otu/sum(otu))
+phylo_rolando_prop <- transform_sample_counts(phylo_rolando, function(otu) otu/sum(otu))
+phylo_both <- merge_phyloseq(phylo_rolando_prop, phylo_rennes_prop)
+colnames(phylo_both@sam_data) <- colnames(phylo_both@sam_data) %>% str_replace("HOST", "Species")
+
+phylo_rennes_prop %>% psmelt() %>% filter(Genus == "Candidatus Thiodiazotropha") %>% nrow()
+phylo_rolando_prop %>% psmelt() %>% filter(Genus == "Candidatus Thiodiazotropha") %>% nrow()
+
+phylo_rennes_prop %>% psmelt() %>% filter(Genus == "Sedimenticola") %>% nrow()
+phylo_rolando_prop %>% psmelt() %>% filter(Genus == "Sedimenticola") %>% nrow()
+
+
+
+
+
+ord.nmds.phylo_both <- ordinate(phylo_both_prop, method="NMDS", distance="bray")
+phylo_rolando_prop@sam_data$
+
+subset_taxa(phylo_both, Family == "Desulfosarcinaceae") %>% 
+  subset_samples(compartment %in% c("root", "Endosphere")) %>%
+  subset_taxa(!(is.na(Family))) %>% 
+  tax_glom("Family") %>%
+  plot_bar(fill="Family") + facet_wrap(~sample_Species, scales="free_x", ncol=3)
+
+phylo_both_melted <- phylo_both %>% psmelt()
+
+phylo_both_melted %>% group_by(study, Genus) %>%
+  summarise(nFeatures = n()) %>% 
+  group_by(Genus) %>% 
+  mutate(sumfeats = sum(nFeatures)) %>%
+  mutate(percent_total = (nFeatures/sumfeats) * 100) %>%
+  data.frame
+
+phylo_both_melted %>% filter(Genus == "Candidatus Thiodiazotropha") %>% group_by(study) %>% summarise(nFeatures = n())
+
+
+phylo_both_melted %>%#
+  filter(Genus == "Candidatus Thiodiazotropha") %>%
+  group_by(study) %>%
+  summarise(nFeatures = n()) 
+  
+phylo_both_melted %>% filter(study == "eur" & Genus == "Candidatus Thiodiazotropha") %>% nrow()
+phylo_both_melted %>% filter(study == "usa" & Genus == "Candidatus Thiodiazotropha") %>% nrow()
+
+phylo_both_melted %>% filter(study == "eur" & Genus == "Sedimenticola") %>% nrow()
+phylo_both_melted %>% filter(study == "usa" & Genus == "Sedimenticola") %>% nrow()
+
+
+
+phylo_both_melted %>% filter(Genus == "Candidatus Thiodiazotropha" & study == "eur")
+
+# plot ordination plot
+plot_ordination(phylo_both_prop, ord.nmds.phylo_both, color="study", title="Bray NMDS", shape="compartment") + 
+  geom_point(size = 7) +
+  theme(strip.text.x = element_text(size=25),
+        axis.text.x = element_text(size=25),
+        axis.text.y = element_text(size=20),
+        axis.title = element_text(size=25),
+        legend.title = element_blank(),
+        legend.text = element_text(size=20),
+        panel.background = element_blank()) +
+  ggtitle("")
+
