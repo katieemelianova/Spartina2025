23779eb2248273cb7a78ffbd59bdcbc316785967
Katie Emelianova <katieemelianova@Mac.telekom.ip>
2026-02-28 20:27:42 +0100

    updates

--- analyse_photosynq.R
+++ analyse_photosynq.R
@@ -8,15 +8,24 @@ library(car)
 library(lme4)
 library(lsmeans)
 
+read_delim("/Users/katieemelianova/Desktop/Spartina/Spartina2025/spartina_photosynq_results.txt") %>% colnames()
 
-psynq <- read_delim("/Users/katieemelianova/Desktop/Spartina/France2025/photosynq_results.txt") %>%
+psynq <- read_delim("/Users/katieemelianova/Desktop/Spartina/Spartina2025/spartina_photosynq_results.txt") %>%
   dplyr::select(LEF, NPQt, PhiNPQ, Phi2, "Light Intensity (PAR)", PhiNO, `Which sample number?`, `Which species?`, `Which locality?`, `which number measurement?`) %>%
-  rename(sample_id = `Which sample number?`,
+  dplyr::rename(sample_id = `Which sample number?`,
          species = `Which species?`,
          locality = `Which locality?`,
          PAR = `Light Intensity (PAR)`,
          measurement=`which number measurement?`)
 
+psynq$species %<>% 
+  str_replace("anglica", "Sporobolus anglicus") %>%
+  str_replace("alterniflora", "Sporobolus alterniflorus") %>%
+  str_replace("maritima", "Sporobolus maritimus")
+  
+  
+
+# take the average of repeat measurements to get a single value per plant per metric
 mean_psynq <- psynq %>% 
   group_by(sample_id) %>%
   summarize(LEF=mean(LEF),
@@ -31,8 +40,6 @@ mean_psynq <- psynq %>%
 
 
 
-
-
 ####################################
 #       phinpq vs ph2.             #
 ####################################
@@ -41,13 +48,18 @@ mean_psynq <- psynq %>%
 # once you have established a good linear relationship for each species, you can use phi2 to perform a t-test for each species pair
 # need to first transform dta to meet normality assumptions
 
+png("phi2_phinpq.png", height=500, width=500)
 mean_psynq %>% 
   ggplot(aes(x=PhiNPQ, y=Phi2, colour=species)) +
   geom_point(size=4) +
   ylim(0.4, 0.9) +
-  xlim(-0.1, 0.4)
+  xlim(-0.1, 0.4) +
+  theme(axis.title = element_text(size=25),
+        axis.text = element_text(size=17))
+dev.off()
 
 mean_psynq %>%
+  drop_na() %>%
   summarise(correl = cor(Phi2, PhiNPQ))
 # -0.93 correlation pearson for all species combined
 # because there is a good inveerse relationship between Phi2 and PhiNPQ we can 
@@ -61,12 +73,12 @@ mean_psynq %>%
 
 # first use the shapiro test to see if Phi2 is significantly different from normal distribution, removing outliers
 mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% pull(Phi2) %>% shapiro.test
-mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% pull(Phi2) %>% densityPlot()
+#mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% pull(Phi2) %>% densityPlot()
 
-# yes it is, so need to transform
+# yes it is sig diff from normal distribution, so need to transform
 # reflect and sqrt normalise phi2 to make it normal
 mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% mutate(transformed_phi2=sqrt(max(Phi2) - Phi2)) %>% pull(transformed_phi2) %>% shapiro.test 
-mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% mutate(transformed_phi2=sqrt(max(Phi2) - Phi2)) %>% pull(transformed_phi2) %>% densityPlot()
+#mean_psynq %>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% mutate(transformed_phi2=sqrt(max(Phi2) - Phi2)) %>% pull(transformed_phi2) %>% densityPlot()
 
 # lock in the new transformed values
 mean_psynq %<>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% mutate(transformed_phi2=sqrt(max(Phi2) - Phi2))
@@ -74,19 +86,19 @@ mean_psynq %<>% drop_na() %>% filter(Phi2 > 0 & Phi2 < 1) %>% mutate(transformed
 # do a t test for difference between species - pairwise
 
 # maritima - alterniflora
-t.test(x = mean_psynq %>% filter(species == "maritima") %>% pull(transformed_phi2), 
-       y = mean_psynq %>% filter(species == "alterniflora") %>% pull(transformed_phi2), 
+t.test(x = mean_psynq %>% filter(species == "Sporobolus maritimus") %>% pull(transformed_phi2), 
+       y = mean_psynq %>% filter(species == "Sporobolus alterniflorus") %>% pull(transformed_phi2), 
        alternative = c("two.sided"))
 
 # maritima - anglica
-t.test(x = mean_psynq %>% filter(species == "maritima") %>% pull(transformed_phi2), 
-       y = mean_psynq %>% filter(species == "anglica") %>% pull(transformed_phi2), 
+t.test(x = mean_psynq %>% filter(species == "Sporobolus maritimus") %>% pull(transformed_phi2), 
+       y = mean_psynq %>% filter(species == "Sporobolus anglicus") %>% pull(transformed_phi2), 
        alternative = c("two.sided"))
 
 
 # alterniflora - anglica
-t.test(x = mean_psynq %>% filter(species == "anglica") %>% pull(transformed_phi2), 
-       y = mean_psynq %>% filter(species == "alterniflora") %>% pull(transformed_phi2), 
+t.test(x = mean_psynq %>% filter(species == "Sporobolus anglicus") %>% pull(transformed_phi2), 
+       y = mean_psynq %>% filter(species == "Sporobolus alterniflorus") %>% pull(transformed_phi2), 
        alternative = c("two.sided"))
 
 
@@ -96,54 +108,71 @@ t.test(x = mean_psynq %>% filter(species == "anglica") %>% pull(transformed_phi2
 # alterniflora has less going to photosynthesis and maritima has more going to photosynthesis
 # a significant difference betrween parents, and the hybrid has an intermediate efficiency of potosynthesis
 
-mean_psynq %>%
-  ggplot(aes(x=species, y=Phi2)) + 
-  geom_boxplot()
+#png("phi2_per_species_boxplot.png", height=1500, width=1000)
+phi2_boxplot <- mean_psynq %>%
+  mutate(species = case_when(species == "Sporobolus alterniflorus" ~ "S. alterniflorus",
+                             species == "Sporobolus anglicus" ~"S. anglicus",
+                             species == "Sporobolus maritimus" ~ "S. maritimus")) %>%
+  ggplot(aes(x=species, y=Phi2, fill=species)) + 
+  geom_boxplot(lwd=0.8) +
+  scale_fill_manual(values=c("brown2", "palegreen3", "dodgerblue2")) +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust=1),
+        axis.title = element_text(size=30),
+        axis.text = element_text(size=25),
+        legend.position="none",
+        axis.title.x = element_blank())
+#dev.off()
+
+##########################
+#       LEF              #
+##########################
+
+# I had thought that PAR was variable in this protocol but it turns out that its just the ambient light level
+# thats why there is such low variation between the values
+# i had analysed LEF vs PAR in the past but now that there doesnt seem to be much variation (measured or expected) in PAR its not worth us including it
+# instead we can just repeat the analyses performed for the Phi2 - compare LEF between species
 
 
-####################################
-#       LEF vs PAR             #
-####################################
+# first use the shapiro test to see if LEF is significantly different from normal distribution, removing outliers
+mean_psynq %>% drop_na() %>% pull(LEF) %>% shapiro.test
+
+# different so reflect transform, check again
+mean_psynq %>% drop_na() %>% mutate(transformed_LEF=sqrt(max(LEF) - LEF)) %>% pull(transformed_LEF) %>% shapiro.test 
+
+# now shapiro test satisfied, lock in values
+mean_psynq %<>% drop_na() %>% mutate(transformed_LEF=sqrt(max(LEF) - LEF))
+
+# maritima - alterniflora
+t.test(x = mean_psynq %>% filter(species == "Sporobolus maritimus") %>% pull(transformed_LEF), 
+       y = mean_psynq %>% filter(species == "Sporobolus alterniflorus") %>% pull(transformed_LEF), 
+       alternative = c("two.sided"))
+
+# maritima - anglica
+t.test(x = mean_psynq %>% filter(species == "Sporobolus maritimus") %>% pull(transformed_LEF), 
+       y = mean_psynq %>% filter(species == "Sporobolus anglicus") %>% pull(transformed_LEF), 
+       alternative = c("two.sided"))
 
 
-# PAR does not go above 140 or so for maritima but goes higher for the other two species
-# this could point to the leaves being more crispy, it doesnt seem that this is a consistent problem but one which is specific to maritima
-# light might have an issue getting through the leaves of this species
-psynq %>% 
-  filter(PAR > 100 & PAR < 180 & LEF > 30 & LEF < 180) %>%
-  ggplot(aes(x=PAR, y=LEF, colour=species)) +
-  geom_point(size=4) +
-  geom_smooth(method="lm", aes(fill = species), alpha=0.2) + 
-  theme(panel.background = element_blank(),
-        axis.text = element_text(size=15),
-        axis.title = element_text(size=20),
-        legend.title = element_blank(),
-        legend.text = element_text(size=12))
-  
+# alterniflora - anglica
+t.test(x = mean_psynq %>% filter(species == "Sporobolus anglicus") %>% pull(transformed_LEF), 
+       y = mean_psynq %>% filter(species == "Sporobolus alterniflorus") %>% pull(transformed_LEF), 
+       alternative = c("two.sided"))
 
 
-
-# I am using linear mixed effects model to account for the multiple measurementsd taken of the same plant
-# I am removing outliers
-# then I am using lstrends to get and compare the slopes of the dots of each species
-# if I use random effects for measurement and it has a singular warning, can I then agree that there is no need to include it?
-m <- lmer(LEF ~ PAR * species + (1|measurement), data = (psynq %>% filter(PAR > 100 & PAR < 180 & LEF > 30 & LEF < 180)))
-mls <- lstrends(m, "species", var="PAR")
-pairs(mls)
-
-test
-
-qqnorm(resid(m))
-qqline(resid(m))
-
-# no significant difference in slopes
-
-plot(resid(m))
-plot(m)
+lef_boxplot <- mean_psynq %>%
+  mutate(species = case_when(species == "Sporobolus alterniflorus" ~ "S. alterniflorus",
+                             species == "Sporobolus anglicus" ~"S. anglicus",
+                             species == "Sporobolus maritimus" ~ "S. maritimus")) %>%
+  ggplot(aes(x=species, y=LEF, fill=species)) + 
+  geom_boxplot(lwd=0.8) +
+  scale_fill_manual(values=c("brown2", "palegreen3", "dodgerblue2")) +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust=1),
+        axis.title = element_text(size=30),
+        axis.text = element_text(size=25),
+        legend.position="none",
+        axis.title.x = element_blank())
 
 
-
-
-
-
-
+#png("lef_ph2_boxplot.png", height=1000, width=800)
+(lef_boxplot + phi2_boxplot)
+#dev.off()
--- analyse_soil_chemistry.R
+++ analyse_soil_chemistry.R
@@ -4,24 +4,70 @@ library(readr)
 library(stringr)
 library(tidyverse)
 library(ggplot2)
+library(patchwork)
 
-soil <- readxl::read_xlsx("soil_analysis_ph_results.xlsx")
+read_delim("/Users/katieemelianova/Desktop/Spartina/Spartina2025/spartina_photosynq_results.txt") %>% colnames()
+
+soil <- readxl::read_xlsx("/Users/katieemelianova/Desktop/Spartina/Spartina2025/spartina_soil_chemistry_results.xlsx")
 soil <- read_delim("/Users/katieemelianova/Desktop/Spartina/Spartina2025/spartina_photosynq_results.txt") %>% 
-  dplyr::select("Which sample number?", "Which species?") %>%
-  rename(`Which sample number?` = "sample", "Which species?" = "species") %>%
-  mutate(species=case_when(species == "anglica" ~"Sporobolus anglicus",
-                           species == "maritima" ~ "Sporobolus maritimus",
-                           species == "alterniflora" ~ "Sporobolus alterniflorus")) %>%
+  dplyr::select("Which sample number?", "Which species?", "Which locality?") %>%
+  rename("sample" = `Which sample number?`, "species" = "Which species?", "locality" = "Which locality?") %>%
+  mutate(species=case_when(species == "anglica" ~"S. anglicus",
+                           species == "maritima" ~ "S. maritimus",
+                           species == "alterniflora" ~ "S. alterniflorus")) %>%
   right_join(soil) %>%
   mutate(species=case_when(sample %in% c("C1", "C2", "C3") ~ "Control",
                            !(sample %in% c("C1", "C2", "C3")) ~ species)) %>% 
-  drop_na()
+  #drop_na() %>%
+  set_colnames(c("sample", "species", "locality", "labnum", "ph", "EC", "dryweight", "nitrogen", "carbon")) %>%
+  filter(!(is.na(species)))
 
+common_theme <- theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust=1),
+                      legend.position="none",
+                      axis.title.x = element_blank(),
+                      axis.title = element_text(size=30),
+                      axis.text = element_text(size=25))
 
-soil %>% 
-  ggplot(aes(x=species, y=`pH (1:10; w:v; 10 mM CaCl2)`, fill=species)) + 
+nitrogen_plot <- soil %>% 
+  ggplot(aes(x=species, y=nitrogen, fill=species)) + 
   geom_boxplot(width = 0.6) +
-  scale_fill_manual(values=c("grey89", "brown2", "palegreen3", "dodgerblue2"))
+  common_theme +
+  scale_fill_manual(values=c("grey89", "brown2", "palegreen3", "dodgerblue2")) +
+  ylab("Total Nitrogen (g/kg-1)")
   
+carbon_plot <- soil %>% 
+  ggplot(aes(x=species, y=carbon, fill=species)) + 
+  geom_boxplot(width = 0.6) +
+  common_theme +
+  scale_fill_manual(values=c("grey89", "brown2", "palegreen3", "dodgerblue2")) +
+  ylab("Soil Organic Carbon (g/kg-1)")
+
+cnratio_plot <- soil %>% 
+  ggplot(aes(x=species, y=carbon/nitrogen, fill=species)) + 
+  geom_boxplot(width = 0.6) +
+  common_theme +
+  scale_fill_manual(values=c("grey89", "brown2", "palegreen3", "dodgerblue2")) +
+  ylab("Carbon/Nitrogen Ratio")
+
+ph_plot <- soil %>% 
+  ggplot(aes(x=species, y=ph, fill=species)) + 
+  geom_boxplot(width = 0.6) +
+  common_theme +
+  scale_fill_manual(values=c("grey89", "brown2", "palegreen3", "dodgerblue2")) +
+  ylab("pH")
+
+
+
+
+#######################################
+#              composite              #
+#######################################
+
+
+png("soil_chemistry_composite.png", width=1700, height=1000)
+((nitrogen_plot | carbon_plot) / (cnratio_plot | ph_plot))  | plot_spacer() + (lef_boxplot + phi2_boxplot) +
+  plot_layout(widths = c(1, 5))
+dev.off()
+
 
 
